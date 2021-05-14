2b026b3922fcb5db4855ed1674768a56cd0538af
unknown <mwfisher91@gmail.com>
2021-05-13 18:17:09 -0700

    Finished Hangman

--- hangman/src/Hangman.js
+++ hangman/src/Hangman.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { randomWord } from './words';
 import img0 from "./0.jpg";
 import img1 from "./1.jpg";
 import img2 from "./2.jpg";
@@ -17,8 +18,10 @@ class Hangman extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+    const hangWord = randomWord();
+    this.state = { nWrong: 0, nCorrect: 0, guessed: new Set(), answer: hangWord };
     this.handleGuess = this.handleGuess.bind(this);
+    this.restartGame = this.restartGame.bind(this);
   }
 
   /** guessedWord: show current-state of word:
@@ -38,21 +41,38 @@ class Hangman extends Component {
     let ltr = evt.target.value;
     this.setState(st => ({
       guessed: st.guessed.add(ltr),
-      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+      nCorrect: st.nCorrect + (st.answer.includes(ltr) ? 1 : 0)
     }));
   }
 
   /** generateButtons: return array of letter buttons to render */
   generateButtons() {
-    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr, index) => (
       <button
         value={ltr}
         onClick={this.handleGuess}
         disabled={this.state.guessed.has(ltr)}
+        key={index}
       >
         {ltr}
       </button>
     ));
+  }
+
+  displayMain(){
+    if(this.state.nWrong !== this.props.maxWrong && this.state.nCorrect !== this.state.answer.length){
+      return this.generateButtons();
+    } else if (this.state.nCorrect === this.state.answer.length) {
+      return 'You win!';
+    } else {
+      return `You lose! The word was ${this.state.answer}.`
+    }
+  }
+
+  restartGame(){
+    let hangWord = randomWord();
+    this.setState({ nWrong: 0, nCorrect: 0, guessed: new Set(), answer: hangWord });
   }
 
   /** render: render game */
@@ -60,9 +80,14 @@ class Hangman extends Component {
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <img src={this.props.images[this.state.nWrong]} alt={`${this.state.nWrong}/${this.props.maxWrong}`} />
+        <p>Number wrong: {this.state.nWrong}</p>
+        <p className='Hangman-word'>{ this.guessedWord() }</p>
+        <p className='Hangman-btns'>{ this.displayMain() }</p>
+        <br />
+        {(this.state.nWrong === this.props.maxWrong || this.state.nCorrect === this.state.answer.length)
+        ? <button onClick={this.restartGame}>Restart Game</button>
+        : null }
       </div>
     );
   }
